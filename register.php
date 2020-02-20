4a641af415233e9f78546cc2cb2be497dade6e28
pawcio <pawelzarzycki89@gmail.com>
2020-02-20 22:23:17 +0100

    initial comit

---
+++ register.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My pawlix clone Netflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    <body>
+        dziala
+    </body>
+</html>
