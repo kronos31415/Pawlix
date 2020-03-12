2fd440183bf6ac52f1645c0d76becf35c8c38014
pawcio <pawelzarzycki89@gmail.com>
2020-03-12 21:55:03 +0100

    detters for entity class

--- includes/classes/Entity.php
+++ includes/classes/Entity.php
@@ -15,5 +15,21 @@
                 $this->sqlData = $query->fetch(PDO::FETCH_ASSOC);
             }
         }
+
+        public function getId() {
+            return $this->sqlData['id'];
+        }
+
+        public function getName() {
+            return $this->sqlData['name'];
+        }
+
+        public function getThumbnail() {
+            return $this->sqlData['thumbnail'];
+        }
+
+        public function getPreview() {
+            return $this->sqlData['preview'];
+        }
     }
 ?>
