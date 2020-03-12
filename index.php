c54e43856e7508b4bda272a05dacbb4e07ce0239
pawcio <pawelzarzycki89@gmail.com>
2020-03-12 21:54:38 +0100

    include Entity class

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
     require_once("includes/config.php");
     require_once("includes/classes/PreviewProvider.php");
+    require_once("includes/classes/Entity.php");
 
     if(!isset($_SESSION['userLogedIn'])) {
         header("Location: register.php");
@@ -8,5 +9,5 @@
     $userLoggedIn = $_SESSION['userLogedIn'];
 
     $preview = new PreviewProvider($conn, $userLoggedIn);
-    $preview->createPreviewVideo(null);
+    echo $preview->createPreviewVideo(null);
 ?>
