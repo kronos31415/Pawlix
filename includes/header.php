41e346a107d093b9bdacd488d2f8588c4e37425b
pawcio <pawelzarzycki89@gmail.com>
2020-03-13 21:08:58 +0100

    create header

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("includes/config.php");
+    require_once("includes/classes/PreviewProvider.php");
+    require_once("includes/classes/Entity.php");
+
+    if(!isset($_SESSION['userLogedIn'])) {
+        header("Location: register.php");
+    }
+    $userLoggedIn = $_SESSION['userLogedIn'];
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My pawlix clone Netflix</title>
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css"/>
+    </head>
+    
+    <body>
+     <div class='wraper'>
--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-    require_once("includes/config.php");
-    require_once("includes/classes/PreviewProvider.php");
-    require_once("includes/classes/Entity.php");
-
-    if(!isset($_SESSION['userLogedIn'])) {
-        header("Location: register.php");
-    }
-    $userLoggedIn = $_SESSION['userLogedIn'];
+    require_once("includes/header.php");
 
     $preview = new PreviewProvider($conn, $userLoggedIn);
     echo $preview->createPreviewVideo(null);
