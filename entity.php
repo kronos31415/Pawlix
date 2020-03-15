965a8bc2b98cbbdc570198916d89a7ff9236dbbf
pawcio <pawelzarzycki89@gmail.com>
2020-03-15 19:02:10 +0100

    entity page

---
+++ entity.php
@@ -0,0 +1,13 @@
+<?php
+    require_once("includes/header.php");
+
+    if(!isset($_GET['id'])) {
+        ErrorMessage::show('No Id was set.');
+    }
+
+    $entityId = $_GET['id'];
+
+    $entity = new Entity($conn, $entityId);
+    $preview = new PreviewProvider($conn, $userLoggedIn);
+    echo $preview->createPreviewVideo($entity);
+?>
