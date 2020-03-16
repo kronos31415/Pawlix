f0433bc3c136cca5dee2a9d5f8e8b0e3d1fec548
pawcio <pawelzarzycki89@gmail.com>
2020-03-16 23:08:44 +0100

    back button view

--- watch.php
+++ watch.php
@@ -11,6 +11,13 @@ $video->incrementVideo();
 ?>
 
 <div class='watchContainer'>
+
+
+    <div class='videoControls watchNav'>
+        <button ><i class="fas fa-arrow-left"></i></button>
+        <h1><?php echo $video->getTitle();  ?></h1>
+    </div>
+
     <video controls autoplay >
         <source src = "<?php echo $video->getFilePath(); ?>" type='video/mp4'>
     </video>
